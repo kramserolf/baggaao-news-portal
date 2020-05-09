56060789ba6b82480d19e91574e6950240f552af
kramserolf <luffyking482@gmail.com>
2020-05-09 20:02:17 +0800

    change meta facebook

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.master')
 @section('content')
+<meta property="og:url"           content="https://baggaonewsportal.herokuapp.com/news/{{$news->id}}" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="{{$news->title}}" />
+  <meta property="og:description"   content="{{$news->content}}" />
+  <meta property="og:image"         content="https://baggaonewsportal.herokuapp.com/img/news/{{$news->image}}" />
 <section id="view-news">
 <div class="container">
 	<nav aria-label="breadcrumb">
