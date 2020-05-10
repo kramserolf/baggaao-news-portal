93e65b2ed2df99f486d3639be572c7bf09da228e
kramserolf <luffyking482@gmail.com>
2020-05-10 18:19:46 +0800

    change cdn

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,8 +1,5 @@
 <!-- bootstrap jquery -->
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- popper.js -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <!-- bootstrap js -->
